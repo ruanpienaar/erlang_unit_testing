1e841567a3e407f9925a25615431c26797275b91
ruanpienaar <ruan800@gmail.com>
2018-03-29 12:01:56 +0100

    Cleanup - split between Unit test ( simple/small, single unit ) and End-to-End Common test responsibilitie

--- src/unit_testing.erl
+++ src/unit_testing.erl
@@ -1,100 +1,110 @@
 -module(unit_testing).
 
-% Erlang Distribution
--export([
-    start_distrib/1,
-    start_distrib/2,
-    start_distrib/3,
-    stop_distrib/0
-]).
+-include_lib("eunit/include/eunit.hrl").
 
-% Test slave nodes
--export([
-    slaves_setup/1,
-    cleanup_slaves/1
-]).
+% I think Unit tests should not have to deal with Erlang distribution...
+% Erlang Distribution
+% -export([
+%     start_distrib/1,
+%     start_distrib/2,
+%     start_distrib/3,
+%     stop_distrib/0
+% ]).
+
+% I think Unit tests should not have to deal with slaves...
+% % Test slave nodes
+% -export([
+%     slaves_setup/1,
+%     cleanup_slaves/1
+% ]).
 
 % Async code testing
 -export([
     wait_for_next_value/3
 ]).
 
+% I think Unit tests should not have to deal with applications started/stopped
 % Running Applications
+% -export([
+%     stop_extra_applications/0
+% ]).
+
+% Eunit test utilities
 -export([
-    stop_extra_applications/0
+    try_test_fun/1
 ]).
 
 %% @doc This will start the distribution ( shortnames , longnames ) with the supplied Nodename.
 %% @end
 
--spec start_distrib(node()) -> ok.
-start_distrib(Name) ->
-    do_start_distrib([Name]).
+% -spec start_distrib(node()) -> ok.
+% start_distrib(Name) ->
+%     do_start_distrib([Name]).
 
--spec start_distrib(node(), shortnames | longnames) -> ok.
-start_distrib(Name, NameType) ->
-    do_start_distrib([Name, NameType]).
+% -spec start_distrib(node(), shortnames | longnames) -> ok.
+% start_distrib(Name, NameType) ->
+%     do_start_distrib([Name, NameType]).
 
--spec start_distrib(node(), shortnames | longnames, pos_integer()) -> ok.
-start_distrib(Name, NameType, Ticktime) ->
-    do_start_distrib([Name, NameType, Ticktime]).
+% -spec start_distrib(node(), shortnames | longnames, pos_integer()) -> ok.
+% start_distrib(Name, NameType, Ticktime) ->
+%     do_start_distrib([Name, NameType, Ticktime]).
 
-do_start_distrib(A) ->
-    case distrib_already_started(node()) of
-        true ->
-            ok;
-        false ->
-            [] = os:cmd("epmd -daemon"),
-            net_kernel:start(A)
-    end.
+% do_start_distrib(A) ->
+%     case distrib_already_started(node()) of
+%         true ->
+%             ok;
+%         false ->
+%             [] = os:cmd("epmd -daemon"),
+%             net_kernel:start(A)
+%     end.
 
--spec distrib_already_started(node()) -> boolean().
-distrib_already_started('nonode@nohost') ->
-    false;
-distrib_already_started(_) ->
-    true.
+% -spec distrib_already_started(node()) -> boolean().
+% distrib_already_started('nonode@nohost') ->
+%     false;
+% distrib_already_started(_) ->
+%     true.
 
--spec stop_distrib() -> ok | {error, not_allowed | not_found}.
-stop_distrib() ->
-    net_kernel:stop().
+% -spec stop_distrib() -> ok | {error, not_allowed | not_found}.
+% stop_distrib() ->
+%     net_kernel:stop().
 
--spec slaves_setup(list(tuple())) -> list(node()).
-slaves_setup(Slaves) when is_list(Slaves) ->
-    lists:map(
-        fun({H})       -> slave_node_start(H);
-           ({H, N})    -> slave_node_start(H, N);
-           ({H, N, A}) -> slave_node_start(H, N, A)
-        end, Slaves).
+% -spec slaves_setup(list(tuple())) -> list(node()).
+% slaves_setup(Slaves) when is_list(Slaves) ->
+%     lists:map(
+%         fun({H})       -> slave_node_start(H);
+%            ({H, N})    -> slave_node_start(H, N);
+%            ({H, N, A}) -> slave_node_start(H, N, A)
+%         end, Slaves).
 
--spec slave_node_start(inet:hostname()) -> node().
-slave_node_start(Host) ->
-    {ok, SlaveName} = slave:start(Host),
-    SlaveName.
+% -spec slave_node_start(inet:hostname()) -> node().
+% slave_node_start(Host) ->
+%     {ok, SlaveName} = slave:start(Host),
+%     SlaveName.
 
--spec slave_node_start(inet:hostname(), atom() | string()) -> node().
-slave_node_start(Host, Name) ->
-    {ok, SlaveName} = slave:start(Host, Name),
-    SlaveName.
+% -spec slave_node_start(inet:hostname(), atom() | string()) -> node().
+% slave_node_start(Host, Name) ->
+%     {ok, SlaveName} = slave:start(Host, Name),
+%     SlaveName.
 
--spec slave_node_start(inet:hostname(), atom() | string(), string()) -> node().
-slave_node_start(Host, Name, Args) ->
-    {ok, SlaveName} = slave:start(Host, Name, Args),
-    SlaveName.
+% -spec slave_node_start(inet:hostname(), atom() | string(), string()) -> node().
+% slave_node_start(Host, Name, Args) ->
+%     {ok, SlaveName} = slave:start(Host, Name, Args),
+%     SlaveName.
 
--spec cleanup_slaves(list(node())) -> boolean().
-cleanup_slaves(Slaves) ->
-    lists:all(fun(SlaveNodeName) ->
-        case slave:stop(SlaveNodeName) of
-            ok ->
-                true;
-            Reason ->
-                error_logger:error_msg(
-                    "Could not stop slave ~p Reason ~p",
-                    [SlaveNodeName, Reason]
-                ),
-                false
-        end
-    end, Slaves).
+% -spec cleanup_slaves(list(node())) -> boolean().
+% cleanup_slaves(Slaves) ->
+%     lists:all(fun(SlaveNodeName) ->
+%         case slave:stop(SlaveNodeName) of
+%             ok ->
+%                 true;
+%             Reason ->
+%                 error_logger:error_msg(
+%                     "Could not stop slave ~p Reason ~p",
+%                     [SlaveNodeName, Reason]
+%                 ),
+%                 false
+%         end
+%     end, Slaves).
 
 wait_for_next_value(0, _, _) ->
     {error, no_answer};
@@ -139,9 +149,19 @@ wait_for_next_value(Times, F, Expected) ->
 %     dbg:tracer(),
 %     dbg:p(all, call).
 
--spec stop_extra_applications() -> list(ok).
-stop_extra_applications() ->
-    [ ok = application:stop(App) ||
-        {App,_ErtsVsn,_Vsn}
-        <- application:which_applications(), App /= kernel andalso App /= stdlib
-    ].
+% -spec stop_extra_applications() -> list(ok).
+% stop_extra_applications() ->
+%     [ ok = application:stop(App) ||
+%         {App,_ErtsVsn,_Vsn}
+%         <- application:which_applications(), App /= kernel andalso App /= stdlib
+%     ].
+
+try_test_fun(TestFun) ->
+    try
+        TestFun(),
+        true
+    catch
+        C:E ->
+            ?debugFmt("test failed: ~p",[{C,E,erlang:get_stacktrace()}]),
+            false
+    end.
