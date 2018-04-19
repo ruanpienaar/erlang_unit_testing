67433618ffb9fc22bad97bd88685f83823c0fd1b
ruanpienaar <ruan800@gmail.com>
2018-04-19 10:36:49 +0100

    Added setup/X fixture

--- src/unit_testing.erl
+++ src/unit_testing.erl
@@ -14,6 +14,110 @@
     try_test_fun/1
 ]).
 
+% Fixture utilities
+-export([
+    setup/2,
+    setup/3,
+    setup/4,
+    setup/5
+    % foreach
+]).
+
+% @doc mocsk :: {module, [options], [expects]}
+
+% {setup, Setup, Tests | Instantiator}
+% {setup, Setup, Cleanup, Tests | Instantiator}
+% {setup, Where, Setup, Tests | Instantiator}
+% {setup, Where, Setup, Cleanup, Tests | Instantiator}
+setup(Setup, Tests) when is_function(Setup, 0) andalso
+                         is_list(Tests) ->
+    {setup, Setup, Tests};
+setup(Setup, {with, Tests}) when is_function(Setup, 0) andalso
+                                 is_list(Tests) ->
+    {setup, Setup, {with, Tests}};
+setup(Setup, Instantiator) when is_function(Setup, 0) andalso
+                                is_function(Instantiator) ->
+    {with, Setup, Instantiator}.
+
+setup(Setup, Cleanup, Tests) when is_function(Setup, 0) andalso
+                                  is_function(Cleanup, 1) andalso
+                                  is_list(Tests) ->
+    ok.
+
+setup(Setup, Cleanup, Tests, Mocks) when is_function(Setup, 0) andalso
+                                         is_function(Cleanup, 1) andalso
+                                         is_list(Tests) andalso
+                                         is_list(Mocks) ->
+    setup(Setup, Cleanup, Tests, Mocks, false).
+
+setup(Setup, Cleanup, Tests, Mocks, StrickMocks) when is_function(Setup, 0) andalso
+                                                            is_function(Cleanup, 1) andalso
+                                                            is_list(Tests) andalso
+                                                            is_list(Mocks) ->
+    MockMods = lists:sort(proplists:get_keys(Mocks)),
+    %?debugFmt("MockMods ~p",[MockMods]),
+    {setup,
+     fun() ->
+        % Apply mocks
+        lists:foreach(fun({Mod, ModOpts, Expectations}) when is_atom(Mod) andalso
+                                                             is_list(ModOpts) andalso
+                                                             is_list(Expectations) ->
+            ok = meck:new(Mod, ModOpts),
+            lists:foreach(
+                fun({Function, Arity, Response}) ->
+                    ok = meck:expect(Mod, Function, Arity, Response);
+                   ({Function, ExpectFun}) when is_function(ExpectFun) ->
+                    ok = meck:expect(Mod, Function, ExpectFun)
+                end
+            , Expectations)
+        end, Mocks),
+        Setup()
+     end,
+     fun(SetupResponse) ->
+        % Run cleanup
+        Cleanup(SetupResponse),
+        % Cleanup mocks
+        case StrickMocks of
+            true ->
+                % ?debugFmt("Strick meck unload", []),
+                ?assertEqual(
+                    MockMods,
+                    lists:sort(meck:unload())
+                );
+            false ->
+                % ?debugFmt("NON Strick meck unload", []),
+                ?assert(
+                    is_list(meck:unload())
+                )
+        end,
+        % Cleanup links
+        {links, Links} = erlang:process_info(self(), links),
+        % ?debugFmt("LINKS ~p", [Links]),
+        lists:foreach(fun(Pid) ->
+            D = erlang:process_info(Pid, [current_function, registered_name]),
+            % ?debugFmt("LINK Details ~p", [D]),
+            case D of
+                [{current_function, {eunit_proc, _, _}}, _] ->
+                    ok;
+                X ->
+                    ?debugFmt(
+                        " === REMOVE LINK === ~n~p~n",
+                        [X]
+                    ),
+                    true = erlang:unlink(Pid),
+                    true = erlang:exit(Pid, kill)
+            end
+        end, Links)
+     end,
+     % List of tests
+     Tests
+    }.
+
+% {foreach, Where, Setup, Cleanup, [Tests | Instantiator]}
+% {foreach, Setup, Cleanup, [Tests | Instantiator]}
+% {foreach, Where, Setup, [Tests | Instantiator]}
+% {foreach, Setup, [Tests | Instantiator]}
+
 wait_for_match(Times, F, Match) ->
     wait_for_match(Times, F, Match, 25).
 
